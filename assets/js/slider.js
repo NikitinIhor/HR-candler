da7e58175f889964fd927ae252b4d3d1198f316e
IHOR <122112010+NikitinIhor@users.noreply.github.com>
2023-08-27 20:42:10 +0200

    Add files via upload

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -9,7 +9,7 @@
 let swiper = new Swiper('.seven__slider', {
 	// direction: 'vertical',
     loop: true,
-	simulateTouch: true,
+	simulateTouch: false,
 
 	navigation: {
 		// <div class="swiper-button-prev"></div>
@@ -50,68 +50,6 @@ let swiper = new Swiper('.seven__slider', {
 	slidesPerView: 1,
 	// отступы
 	spaceBetween: 200,
-
-	// breakpoints: {
-	// 	320: {
-	// 		slidesPerView:1,
-	// 	},
-	// 	480: {
-	// 		slidesPerView:2,
-	// 	},
-	// 	767.98: {
-	// 		slidesPerView:3,
-	// 	},
-	// 	970: {
-	// 		slidesPerView:3,
-	// 	}
-	// },
-
-});
-
-let mySwiper = new Swiper('.swiper-eight', {
-	// direction: 'vertical',
-    loop: true,
-	simulateTouch: true,
-
-	navigation: {
-		// <div class="swiper-button-prev"></div>
-        // <div class="swiper-button-next"></div>
-
-		nextEl: '.swiper-button-prev',
-		prevEl: '.swiper-button-next'
-	},
-
-	pagination: {
-		// <div class="swiper-pagination"></div>
-
-		// точки
-		type: 'bullets',
-		
-		// цифры
-		// type: 'fraction',
-		renderFraction: function (currentClass, totalClass) {
-			return '0<span class="' + currentClass + '"></span>'+
-			'/' +
-			'<span class="' + totalClass + '"></span>';
-		},
-		
-		el:'.swiper-pagination',
-
-        // булеты(можно кликать)
-		clickable: true,
-		dynamicBullets: true,
-	},
-
-	scrollbar: {
-		el: '.swiper-scrollbar',
-
-		draggable: true
-	},
-
-	// кол-во слайдов
-	slidesPerView: 3,
-	// отступы
-	spaceBetween:20,
 
 	// breakpoints: {
 	// 	320: {
