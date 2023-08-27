bf038e988cd21d6728db4d6fb18bbaf27de94a60
IHOR <122112010+NikitinIhor@users.noreply.github.com>
2023-08-27 11:43:40 +0200

    Add files via upload

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -6,10 +6,9 @@
 // SCSS SLIDER !!!!!!!!!!!!!!
 
 
-
-new Swiper('.имя блока__slider', {
+let swiper = new Swiper('.seven__slider', {
 	// direction: 'vertical',
-    // loop: true,
+    loop: true,
 	simulateTouch: true,
 
 	navigation: {
@@ -24,10 +23,10 @@ new Swiper('.имя блока__slider', {
 		// <div class="swiper-pagination"></div>
 
 		// точки
-		// type: 'bullets',
+		type: 'bullets',
 		
 		// цифры
-		type: 'fraction',
+		// type: 'fraction',
 		renderFraction: function (currentClass, totalClass) {
 			return '0<span class="' + currentClass + '"></span>'+
 			'/' +
@@ -48,9 +47,71 @@ new Swiper('.имя блока__slider', {
 	},
 
 	// кол-во слайдов
-	slidesPerView: 1.1,
+	slidesPerView: 1,
 	// отступы
-	spaceBetween: 0,
+	spaceBetween: 200,
+
+	// breakpoints: {
+	// 	320: {
+	// 		slidesPerView:1,
+	// 	},
+	// 	480: {
+	// 		slidesPerView:2,
+	// 	},
+	// 	767.98: {
+	// 		slidesPerView:3,
+	// 	},
+	// 	970: {
+	// 		slidesPerView:3,
+	// 	}
+	// },
+
+});
+
+let mySwiper = new Swiper('.swiper-eight', {
+	// direction: 'vertical',
+    loop: true,
+	simulateTouch: true,
+
+	navigation: {
+		// <div class="swiper-button-prev"></div>
+        // <div class="swiper-button-next"></div>
+
+		nextEl: '.swiper-button-prev',
+		prevEl: '.swiper-button-next'
+	},
+
+	pagination: {
+		// <div class="swiper-pagination"></div>
+
+		// точки
+		type: 'bullets',
+		
+		// цифры
+		// type: 'fraction',
+		renderFraction: function (currentClass, totalClass) {
+			return '0<span class="' + currentClass + '"></span>'+
+			'/' +
+			'<span class="' + totalClass + '"></span>';
+		},
+		
+		el:'.swiper-pagination',
+
+        // булеты(можно кликать)
+		clickable: true,
+		dynamicBullets: true,
+	},
+
+	scrollbar: {
+		el: '.swiper-scrollbar',
+
+		draggable: true
+	},
+
+	// кол-во слайдов
+	slidesPerView: 3,
+	// отступы
+	spaceBetween:20,
 
 	// breakpoints: {
 	// 	320: {
